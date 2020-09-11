511302a02cfafb22a2e10ef8b80df121e0a697e3
Shadow <ShadowZhang@sina.com>
2020-09-11 10:36:19 +0800

    add service.sh

---
+++ service.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+PROG=MicroService-EmployeeService
+APPPATH=/tpc/webapps/microservice/employee
+JARNAME=MicroService-EmployeeService-1.0.0.jar
+PIDFILE=$APPPATH/$PROG.pid
+cd $APPPATH
+
+status() {
+    if [ -f $PIDFILE ]; then  
+        PID=$(cat $PIDFILE)  
+        if [ ! -x /proc/${PID} ]; then  
+            return 1  
+        else  
+            return 0  
+        fi  
+    else  
+        return 1  
+    fi  
+}  
+  
+case "$1" in  
+    start)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            echo "$PIDFILE exists, process is already running or crashed"  
+            exit 1  
+        fi  
+          
+        echo "Starting $PROG ..."  
+		    nohup /tpc/jdk1.8/bin/java -jar $APPPATH/$JARNAME -Dservice=$PROG >> $APPPATH/$PROG.log 2>&1 & 
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            echo "$PROG is started"  
+            echo $! > $PIDFILE  
+            exit 0  
+        else  
+            echo "Stopping $PROG"  
+            rm -f $PIDFILE  
+            exit 1  
+        fi  
+        ;;  
+    stop)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            echo "Shutting down $PROG"  
+            kill `cat $PIDFILE`  
+            RETVAL=$?  
+            if [ $RETVAL -eq 0 ]; then  
+                rm -f $PIDFILE  
+            else  
+                echo "Failed to stopping $PROG"  
+            fi  
+        fi  
+        ;;  
+    status)  
+        status  
+        RETVAL=$?  
+        if [ $RETVAL -eq 0 ]; then  
+            PID=$(cat $PIDFILE)  
+            echo "$PROG is running ($PID)"  
+        else  
+            echo "$PROG is not running"  
+        fi  
+        ;;  
+    restart)  
+        $0 stop  
+        $0 start  
+        ;;  
+    *)  
+        echo "Usage: $0 {start|stop|restart|status}"  
+        ;;  
+esac 
