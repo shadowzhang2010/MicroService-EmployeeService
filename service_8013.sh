a65cfdb2366eeb0dca4a30693c91d0b641e799d5
Shadow <ShadowZhang@sina.com>
2020-09-11 12:49:45 +0800

    add more ports

--- service_8013.sh
+++ service_8013.sh
@@ -29,7 +29,7 @@ case "$1" in
         fi  
           
         echo "Starting $PROG ..."  
-		    nohup /tpc/jdk1.8/bin/java -Dserver.port=8011 -jar $APPPATH/$JARNAME -Dservice=$PROG >> $APPPATH/$PROG.log 2>&1 & 
+		    nohup /tpc/jdk1.8/bin/java -Dserver.port=8013 -jar $APPPATH/$JARNAME -Dservice=$PROG >> $APPPATH/$PROG_8013.log 2>&1 & 
         RETVAL=$?  
         if [ $RETVAL -eq 0 ]; then  
             echo "$PROG is started"  
